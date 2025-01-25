655f32c6ec63b6fba7c1d63ba022c5b262ae7b8e
Karan <mrkaran2k5@gmail.com>
2025-01-25 21:20:31 +0530

    Basic Home Page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,12 @@
+import type { Metadata } from "next";
+import HomePage from "@/components/HomePage";
+
+export const metadata: Metadata = {
+  title: "Welcome to Campus Diary - Empowering Minds, Shaping Futures",
+  description:
+    "Discover a world-class education at College Name. Explore our programs, campus life, and opportunities for personal and professional growth.",
+};
 
 export default function Home() {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <HomePage />;
 }
