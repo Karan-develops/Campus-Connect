43e8b82314d77c24a576e75d95c95203711a983d
Karan <mrkaran2k5@gmail.com>
2025-02-15 11:37:11 +0530

    Custom error page

---
+++ frontend/app/error.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { Undo2 } from "lucide-react";
+import Link from "next/link";
+import { useEffect } from "react";
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error("Error caught:", error);
+  }, [error]);
+
+  return (
+    <div className="flex items-center min-h-screen px-4 py-12 sm:px-6 md:px-8 lg:px-12 xl:px-16">
+      <div className="w-full space-y-6 text-center">
+        <div className="space-y-3">
+          <h1 className="text-4xl text-red-500 font-bold tracking-tighter sm:text-5xl animate-bounce">
+            Something went wrong! - 404
+          </h1>
+          <p className="text-lg mt-2">Error : {error.message}</p>
+          <p className="text-gray-500">
+            Looks like you've ventured into the unknown digital realm.
+          </p>
+        </div>
+        <Link
+          href={"/"}
+          className="inline-flex h-10 items-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+          prefetch={false}
+        >
+          Return to Home Page
+          <Undo2 className="p-1 size-7" />
+        </Link>
+      </div>
+    </div>
+  );
+}
--- frontend/components/HomePage.tsx
+++ frontend/components/HomePage.tsx
@@ -6,7 +6,7 @@ import Hero2 from "./Hero2";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen overflow-x-hidden">
       <HeroGeometric />
       <Hero2/>
       <Marquee />
